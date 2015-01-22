8e45866fdaaa98860ced904e61c17acd5b5097bd
Lukasz Kuczborski <lukasz.kuczborski@allegrogroup.com>
2015-01-22 14:29:48 +0100

    Initial work on color processing

--- Colors/Colors/ViewController.h
+++ Colors/Colors/ViewController.h
@@ -7,9 +7,9 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "BeaconHandler.h"
 
-@interface ViewController : UIViewController
-
+@interface ViewController : UIViewController <BeaconEventHandler>
 
 @end
 
--- Colors/Colors/ViewController.m
+++ Colors/Colors/ViewController.m
@@ -7,12 +7,47 @@
 //
 
 #import "ViewController.h"
+#import "BeaconHandler.h"
 
 @interface ViewController ()
+
+@property Beacon *rBeacon;
+@property Beacon *gBeacon;
+@property Beacon *bBeacon;
 
 @end
 
 @implementation ViewController
+
+- (void)beaconHandler:(BeaconHandler *)handler
+     didUpdateBeacons:(NSArray *)beacons {
+    UIColor *currentColor = [UIColor colorWithCGColor:self.view.layer.backgroundColor];
+    CGFloat r = 0.f;
+    CGFloat g = 0.f;
+    CGFloat b = 0.f;
+    CGFloat a = 1.f;
+    [currentColor getRed:&r green:&g blue:&b alpha:&a];
+    for (Beacon *beacon in beacons) {
+        if ([beacon.name isEqualToString:self.rBeacon.name]) {
+            r = [self getComponentValueFromBeacon:_rBeacon];
+        } else if ([beacon.name isEqualToString:self.gBeacon.name]) {
+            r = [self getComponentValueFromBeacon:_bBeacon];
+        } else if ([beacon.name isEqualToString:self.bBeacon.name]) {
+            r = [self getComponentValueFromBeacon:_gBeacon];
+        }
+    }
+    [self animateToColor:[UIColor colorWithRed:r green:g blue:b alpha:a]];
+}
+
+- (CGFloat)getComponentValueFromBeacon:(Beacon *)beacon {
+    return 0.f;
+}
+
+- (void)animateToColor:(UIColor*)color {
+    [UIView animateWithDuration:0.5 animations:^{
+        self.view.layer.backgroundColor = color.CGColor;
+    } completion:NULL];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
