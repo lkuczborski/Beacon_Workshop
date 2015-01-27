d3d79cd74ed55562ceec50e6418fea3d20e6eccc
Lukasz Stocki <lukasz.stocki@allegrogroup.com>
2015-01-27 10:35:44 +0100

    Initial beacons code.

--- CTF/CTF/ViewController.m
+++ CTF/CTF/ViewController.m
@@ -8,7 +8,13 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+#import <BeaconKit/BeaconKit.h>
+
+static NSString *const HUE_1 = @"430F6ED4-7E4F-4F63-85C7-845763861A55";
+static NSString *const HUE_2 = @"B9407F30-F5F8-466E-AFF9-25556B57FE6D";
+static NSString *const HUE_3 = @"3A7E7514-A9AC-41F3-A236-3CFCE75BAC95";
+
+@interface ViewController () <BeaconEventHandler>
 
 @property (weak, nonatomic) IBOutlet UIView *currentTeam;
 
@@ -32,4 +38,23 @@
     
     self.currentTeam.backgroundColor = sender.backgroundColor;
 }
+
+#pragma mark - Hue Changes
+- (void)changeBulbHueWithBeacon:(Beacon *)beacon
+{
+    
+}
+
+
+#pragma mark - BeaconEventHandler
+- (void)beaconHandler:(BeaconHandler *)handler
+     didUpdateBeacons:(NSArray *)beacons {
+    
+    for (Beacon *beacon in beacons) {
+        if (beacon.proximity == CLProximityImmediate) {
+            [self changeBulbHueWithBeacon:beacon];
+        }
+    }
+}
+
 @end
