a107fde265ebe4201a87bc6d82f36b77b62f969b
Lukasz Stocki <lukasz.stocki@allegrogroup.com>
2015-01-27 10:38:37 +0100

    Initial beacons code.

--- CTF/CTF/ViewController.m
+++ CTF/CTF/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 #import <BeaconKit/BeaconKit.h>
+#import <HueSDK_iOS/HueSDK.h>
 
 static NSString *const HUE_1 = @"430F6ED4-7E4F-4F63-85C7-845763861A55";
 static NSString *const HUE_2 = @"B9407F30-F5F8-466E-AFF9-25556B57FE6D";
@@ -22,13 +23,17 @@ static NSString *const HUE_3 = @"3A7E7514-A9AC-41F3-A236-3CFCE75BAC95";
 @property (weak, nonatomic) IBOutlet UIView *hue2;
 @property (weak, nonatomic) IBOutlet UIView *hue3;
 
+@property (nonatomic, strong) BeaconHandler *handler;
+
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+    [self setupBeaconHandler];
 }
 
 
@@ -43,6 +48,24 @@ static NSString *const HUE_3 = @"3A7E7514-A9AC-41F3-A236-3CFCE75BAC95";
 - (void)changeBulbHueWithBeacon:(Beacon *)beacon
 {
     
+}
+
+#pragma mark - BeaconHandler
+
+- (void)setupBeaconHandler {
+    NSArray *regions = @[
+                         [[BeaconRegion alloc] initWithUUID:HUE_1
+                                                       name:@"Estimote Mint Cocktail"],
+                         [[BeaconRegion alloc] initWithUUID:HUE_2
+                                                       name:@"Estimote Icy Marshmallow"],
+                         [[BeaconRegion alloc] initWithUUID:HUE_3
+                                                       name:@"Estimote Blueberry Pie"]
+                         ];
+    
+    self.handler = [[BeaconHandler alloc] initWithBeaconRegions:regions
+                                                       delegate:self];
+    
+    [self.handler start];
 }
 
 
