84762507e41053a1254168104c50e3ab17efcf8b
Lukasz Stocki <lukasz.stocki@allegrogroup.com>
2015-01-29 14:48:37 +0100

    Adds comments to the BeaconRegion header file.

--- BeaconKit/BeaconKit/BeaconRegion.h
+++ BeaconKit/BeaconKit/BeaconRegion.h
@@ -8,11 +8,22 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ *  Class reperesnting beacon region.
+ */
 @interface BeaconRegion : NSObject
 
 @property (nonatomic, copy) NSString *uuid;
 @property (nonatomic, copy) NSString *name;
 
+/**
+ *  Designated initalizer for creating a new BeaconRegion instance.
+ *
+ *  @param uuid NSString with the UUID for reagion.
+ *  @param name Unique name for region.
+ *
+ *  @return New instance of BeaconRegion.
+ */
 - (instancetype)initWithUUID:(NSString *)uuid
                         name:(NSString *)name NS_DESIGNATED_INITIALIZER;
 
