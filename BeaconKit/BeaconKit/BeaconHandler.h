c0e5a1d881599fe7f2276ef14271be8184466757
Lukasz Stocki <lukasz.stocki@allegrogroup.com>
2015-01-29 14:42:12 +0100

    Adds comments to the BeaconHandler header file.

--- BeaconKit/BeaconKit/BeaconHandler.h
+++ BeaconKit/BeaconKit/BeaconHandler.h
@@ -17,18 +17,38 @@
 
 @protocol BeaconEventHandler <NSObject>
 
+/**
+ *  Called on BeaconHandler delegate when some beacons were updated.
+ *
+ *  @param handler BeaconHandler instance that updated it's beacons.
+ *  @param beacons NSArray of Beacon instances that were updated.
+ */
 - (void)beaconHandler:(BeaconHandler *)handler
      didUpdateBeacons:(NSArray *)beacons;
 
 @end
 
+/**
+ *  Class that encapsulates logic required for beacon magic to work.
+ */
 @interface BeaconHandler : NSObject
 
 @property (nonatomic, weak) id <BeaconEventHandler> delegate;
 
+/**
+ *  Designated initalizer for creating a BeaconManager.
+ *
+ *  @param regions  NSArray of BeaconRegion instancess defining regions in whitch this manager should handle beacons.
+ *  @param delegate Delegate that will be notified about updated beacons.
+ *
+ *  @return Instance of BeaconHandler.
+ */
 - (instancetype)initWithBeaconRegions:(NSArray *)regions
                              delegate:(id <BeaconEventHandler>)delegate NS_DESIGNATED_INITIALIZER;
 
+/**
+ *  Starts monitoring for beacons in defined regions.
+ */
 - (void)start;
 
 @end
