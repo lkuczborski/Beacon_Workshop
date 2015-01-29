75fb2d28cb75762569cf1d8f322d6ba3d7952bdd
Lukasz Stocki <lukasz.stocki@allegrogroup.com>
2015-01-29 11:16:37 +0100

    Changes bulb colour according to the team colour.

--- CTF/CTF/ViewController.m
+++ CTF/CTF/ViewController.m
@@ -19,6 +19,11 @@ static NSString *const HUE_1 = @"430F6ED4-7E4F-4F63-85C7-845763861A55";
 static NSString *const HUE_2 = @"B9407F30-F5F8-466E-AFF9-25556B57FE6D";
 static NSString *const HUE_3 = @"3A7E7514-A9AC-41F3-A236-3CFCE75BAC95";
 
+static NSString *const HUE_1_NAME = @"Hue Lamp";
+static NSString *const HUE_2_NAME = @"Hue Lamp 1";
+static NSString *const HUE_3_NAME = @"Hue Lamp 2";
+
+
 @interface ViewController () <BeaconEventHandler>
 
 @property (weak, nonatomic) IBOutlet UIView *currentTeam;
@@ -29,11 +34,22 @@ static NSString *const HUE_3 = @"3A7E7514-A9AC-41F3-A236-3CFCE75BAC95";
 
 @property (nonatomic, strong) NSMutableDictionary *beaconToBulbPairing;
 
+@property (nonatomic, strong) NSDictionary *hueIDToName;
+@property (nonatomic, strong) NSDictionary *nameToHueID;
+
 @property (nonatomic, strong) BeaconHandler *handler;
 
 @end
 
 @implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    self.hueIDToName = @{HUE_1:HUE_1_NAME, HUE_2:HUE_2_NAME, HUE_3:HUE_3_NAME};
+    self.nameToHueID = @{HUE_1_NAME:HUE_1, HUE_2_NAME:HUE_2, HUE_3_NAME:HUE_3};
+}
 
 - (void)viewDidAppear:(BOOL)animated
 {
@@ -169,7 +185,14 @@ static NSString *const HUE_3 = @"3A7E7514-A9AC-41F3-A236-3CFCE75BAC95";
 #pragma mark - Hue Changes
 - (void)changeBulbHueWithBeacon:(Beacon *)beacon
 {
+    PHLight *lightToChange = [self lightForBeacon:beacon];
     
+    if (lightToChange == nil) {
+        NSLog(@"%s\t No lihgt bulb to change :(", __PRETTY_FUNCTION__);
+        return;
+    }
+    
+    [self updateLight:lightToChange withColor:self.currentTeam.backgroundColor];
 }
 
 #pragma mark - BeaconHandler
@@ -200,6 +223,109 @@ static NSString *const HUE_3 = @"3A7E7514-A9AC-41F3-A236-3CFCE75BAC95";
             [self changeBulbHueWithBeacon:beacon];
         }
     }
+}
+
+#pragma mark - Helper Methods
+- (NSInteger)calculateNewHueStepFromLightState:(PHLightState *)lightState toColor:(UIColor *)newColor {
+    PHLightState *currentColor = lightState;
+    NSInteger currentHue = [[currentColor hue] integerValue];
+    NSInteger currentSaturation = [[currentColor saturation] integerValue];
+    
+    CGFloat hue, saturation;
+    [newColor getHue:&hue saturation:&saturation brightness:nil alpha:nil];
+    
+    NSInteger targetHue = hue * MAX_HUE;
+    NSInteger targetSaturation = saturation * 254;
+    
+    NSInteger newHue = currentHue;
+    NSInteger newSaturation = 0;
+    
+    if (currentHue != targetHue) {
+        if (currentHue > targetHue) {
+            newHue = currentHue - HUE_STEP;
+            if (newHue < targetHue)
+                newHue = targetHue;
+            if (newHue < 0)
+                newHue = 0;
+        } else {
+            newHue = currentHue + HUE_STEP;
+            if (newHue > targetHue)
+                newHue = targetHue;
+            if (newHue > MAX_HUE)
+                newHue = MAX_HUE;
+        }
+    }
+    
+    if (currentSaturation != targetSaturation) {
+        if (currentSaturation > targetSaturation) {
+            newSaturation = currentSaturation - SATURATION_STEP;
+            if (newSaturation < targetSaturation)
+                newSaturation = targetSaturation;
+            if (newSaturation < 0)
+                newSaturation = 0;
+        } else {
+            newSaturation = currentSaturation + SATURATION_STEP;
+            if (newSaturation > targetSaturation)
+                newSaturation = targetSaturation;
+            if (newSaturation > 254) //MAKE MAX_SATURATION
+                newSaturation = 254;
+        }
+    }
+    
+    return newHue;
+}
+
+- (void)updateLight:(PHLight *)light withColor:(UIColor *)newColor {
+    
+    NSInteger hueToSet = [self calculateNewHueStepFromLightState:light.lightState
+                                                         toColor:newColor];
+    
+    PHLightState *newLightState = [self lightStateForHue:hueToSet];
+    
+    [self updateLight:light withState:newLightState];
+}
+
+- (PHLightState *)lightStateForHue:(NSInteger)hue {
+    
+    PHLightState *newLightState = [[PHLightState alloc] init];
+    [newLightState setHue:@(hue)];
+    [newLightState setSaturation:@(254)];
+    [newLightState setBrightness:@(0)];
+    
+    return newLightState;
+}
+
+- (void)updateLight:(PHLight *)light withState:(PHLightState *)newState {
+    
+    PHBridgeSendAPI *sendApi = [[PHBridgeSendAPI alloc] init];
+    
+    [sendApi updateLightStateForId:light.identifier
+                    withLightState:newState
+                 completionHandler:^(NSArray *errors) {
+                     if (errors != nil) {
+                         NSString *message = [NSString stringWithFormat:@"%@: %@", NSLocalizedString(@"Errors", @""), errors != nil ? errors : NSLocalizedString(@"none", @"")];
+                         
+                         NSLog(@"Response: %@",message);
+                     }
+                     
+                 }];
+}
+
+- (PHLight *)lightForBeacon:(Beacon *)beacon {
+    
+    PHBridgeResourcesCache *cache = [PHBridgeResourcesReader readBridgeResourcesCache];
+    
+    NSString *beaconID = beacon.uuid;
+    NSString *bulbName = self.hueIDToName[beaconID];
+
+    for (PHLight *lightBulb in cache.lights.allValues) {
+
+        if ([lightBulb.name isEqualToString:bulbName]) {
+            return lightBulb;
+        }
+    }
+    
+    return nil;
 }
 
 @end
