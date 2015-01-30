ff8022cfe349708240cfa4f7b1d9dc2d20a7ec81
Lukasz Kuczborski <lukasz.kuczborski@allegrogroup.com>
2015-01-30 08:47:34 +0100

    Project setup, Framework changed back to static library

--- BeaconKit/BeaconKit/BeaconKit.h
+++ BeaconKit/BeaconKit/BeaconKit.h
@@ -2,20 +2,15 @@
 //  BeaconKit.h
 //  BeaconKit
 //
-//  Created by Lukasz Stocki on 28.01.2015.
+//  Created by Lukasz Kuczborski on 30.01.2015.
 //  Copyright (c) 2015 Allegro Group. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-
-//! Project version number for BeaconKit.
-FOUNDATION_EXPORT double BeaconKitVersionNumber;
-
-//! Project version string for BeaconKit.
-FOUNDATION_EXPORT const unsigned char BeaconKitVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <BeaconKit/PublicHeader.h>
-
+#import <Foundation/Foundation.h>
 #import "Beacon.h"
 #import "BeaconHandler.h"
 #import "BeaconRegion.h"
+
+@interface BeaconKit : NSObject
+
+@end
---
+++ BeaconKit/BeaconKit/BeaconKit.m
@@ -0,0 +1,13 @@
+//
+//  BeaconKit.m
+//  BeaconKit
+//
+//  Created by Lukasz Kuczborski on 30.01.2015.
+//  Copyright (c) 2015 Allegro Group. All rights reserved.
+//
+
+#import "BeaconKit.h"
+
+@implementation BeaconKit
+
+@end
