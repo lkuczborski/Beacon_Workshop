62af8f6dc7370edfa552c7a86071fa9fc320dd81
Lukasz Stocki <lukasz.stocki@allegrogroup.com>
2016-02-10 23:28:09 +0100

    Adds a FAT todo ;)

--- ProkKit/ProkKit/Beacon.swift
+++ ProkKit/ProkKit/Beacon.swift
@@ -42,6 +42,108 @@ struct Beacon {
                  major: CLBeaconMajorValue(beacon.major.integerValue),
                  minor: CLBeaconMinorValue(beacon.minor.integerValue))
     }
+
+
+
+
+// TODO:
+// Convert this code in to a Swift nice code
+
+//    #pragma mark - Muttators
+//    #pragma mark Accuracy
+//    - (void)setAccuracy:(CLLocationAccuracy)accuracy
+//    {
+//    if ([@(_accuracy) compare:@(accuracy)] == NSOrderedSame) {
+//    return;
+//    }
+//
+//    if ([@(accuracy) compare:@(-1)] == NSOrderedSame) {
+//    return;
+//    }
+//
+//    _accuracy = accuracy;
+//
+//    [self updateMaxAccuracyWithAccuracy:accuracy];
+//    [self updateMinAccuracyWithAccuracy:accuracy];
+//    }
+//
+//    - (void)updateMaxAccuracyWithAccuracy:(CLLocationAccuracy)accuracy
+//    {
+//    if ([@(self.accuracyMax) compare:@(accuracy)] == NSOrderedDescending) {
+//    self.accuracyMax = accuracy;
+//    }
+//    }
+//
+//    - (void)updateMinAccuracyWithAccuracy:(CLLocationAccuracy)accuracy
+//    {
+//    if ([@(self.accuracyMin) compare:@(accuracy)] == NSOrderedAscending) {
+//    self.accuracyMin = accuracy;
+//    }
+//    }
+//
+//    - (CLLocationAccuracy)accuracyMax
+//    {
+//    if (self.customAccuracyMax) {
+//    return [self.customAccuracyMax doubleValue];
+//    }
+//
+//    return _accuracyMax;
+//    }
+//
+//    - (CLLocationAccuracy)accuracyMin
+//    {
+//    if (self.customAccuracyMin) {
+//    return [self.customAccuracyMin doubleValue];
+//    }
+//
+//    return _accuracyMin;
+//    }
+//
+//    #pragma mark RSSI
+//    - (void)setRssi:(NSInteger)rssi
+//    {
+//    if (_rssi == rssi || rssi == 0) {
+//    return;
+//    }
+//
+//    _rssi = rssi;
+//    [self updateMaxRSSIWithRSSI:rssi];
+//    [self updateMinRSSIWithRSSI:rssi];
+//    }
+//
+//    - (void)updateMaxRSSIWithRSSI:(NSInteger)rssi
+//    {
+//    if (self.rssiMax < rssi) {
+//    self.rssiMax = rssi;
+//    }
+//    }
+//
+//    - (void)updateMinRSSIWithRSSI:(NSInteger)rssi
+//    {
+//    if (self.rssiMin > rssi) {
+//    self.rssiMin = rssi;
+//    }
+//    }
+//
+//    - (NSInteger)rssiMax
+//    {
+//    if (self.customRSSIMax) {
+//    return [self.customRSSIMax integerValue];
+//    }
+//    
+//    return _rssiMax;
+//    }
+//    
+//    - (NSInteger)rssiMin
+//    {
+//    if (self.customRSSIMin) {
+//    return [self.customRSSIMin integerValue];
+//    }
+//    
+//    return _rssiMin;
+//    }
+
+
 }
 
 extension Beacon: CustomStringConvertible {
