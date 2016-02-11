7f35a896730849e3556bdcafc45710d7421c2237
Lukasz Stocki <lukasz.stocki@allegrogroup.com>
2016-02-11 18:49:39 +0100

    Simplifies things.

--- ProkKit/ProkKit/Beacon.swift
+++ ProkKit/ProkKit/Beacon.swift
@@ -10,31 +10,34 @@ import Foundation
 import CoreLocation;
 
 struct Beacon {
-    private(set) var uuid        : String
-    private(set) var major       : CLBeaconMajorValue
-    private(set) var minor       : CLBeaconMinorValue
-    private(set) var proximity   : CLProximity
 
-    private(set) var accuracy          : CLLocationAccuracy
-    private(set) var accuracyMax       : CLLocationAccuracy?
-    private(set) var accuracyMin       : CLLocationAccuracy?
-    private(set) var customAccuracyMax : CLLocationAccuracy?
-    private(set) var customAccuracyMin : CLLocationAccuracy?
+    private(set) var major    : CLBeaconMajorValue
+    private(set) var minor    : CLBeaconMinorValue
+    private(set) var proximity: CLProximity
+    private(set) var rssi     : Int
+    private(set) var uuid     : String
 
-    private(set) var rssi          : NSNumber
-    private(set) var rssiMax       : NSNumber?
-    private(set) var rssiMin       : NSNumber?
-    private(set) var customRssiMax : NSNumber?
-    private(set) var customRssiMin : NSNumber?
+    private(set) var accuracy: CLLocationAccuracy {
+        set {
+            if case let newAccuracy = newValue where newAccuracy != self.accuracy && newAccuracy != -1 {
+                self.accuracy = newAccuracy
+            }
+        }
+
+        get {
+            return self.accuracy
+        }
+    }
 
     init(uuid: String, major: CLBeaconMajorValue, minor: CLBeaconMinorValue) {
+
         self.uuid  = uuid
         self.major = major
         self.minor = minor
 
-        self.accuracy = -1
-        self.rssi = 0
-        self.proximity = .Unknown
+        proximity = .Unknown
+        rssi      = 0
+        accuracy  = -1;
     }
 
     init(beacon: CLBeacon) {
@@ -42,106 +45,6 @@ struct Beacon {
                  major: CLBeaconMajorValue(beacon.major.integerValue),
                  minor: CLBeaconMinorValue(beacon.minor.integerValue))
     }
-
-
-
-
-// TODO:
-// Convert this code in to a Swift nice code
-
-//    #pragma mark - Muttators
-//    #pragma mark Accuracy
-//    - (void)setAccuracy:(CLLocationAccuracy)accuracy
-//    {
-//    if ([@(_accuracy) compare:@(accuracy)] == NSOrderedSame) {
-//    return;
-//    }
-//
-//    if ([@(accuracy) compare:@(-1)] == NSOrderedSame) {
-//    return;
-//    }
-//
-//    _accuracy = accuracy;
-//
-//    [self updateMaxAccuracyWithAccuracy:accuracy];
-//    [self updateMinAccuracyWithAccuracy:accuracy];
-//    }
-//
-//    - (void)updateMaxAccuracyWithAccuracy:(CLLocationAccuracy)accuracy
-//    {
-//    if ([@(self.accuracyMax) compare:@(accuracy)] == NSOrderedDescending) {
-//    self.accuracyMax = accuracy;
-//    }
-//    }
-//
-//    - (void)updateMinAccuracyWithAccuracy:(CLLocationAccuracy)accuracy
-//    {
-//    if ([@(self.accuracyMin) compare:@(accuracy)] == NSOrderedAscending) {
-//    self.accuracyMin = accuracy;
-//    }
-//    }
-//
-//    - (CLLocationAccuracy)accuracyMax
-//    {
-//    if (self.customAccuracyMax) {
-//    return [self.customAccuracyMax doubleValue];
-//    }
-//
-//    return _accuracyMax;
-//    }
-//
-//    - (CLLocationAccuracy)accuracyMin
-//    {
-//    if (self.customAccuracyMin) {
-//    return [self.customAccuracyMin doubleValue];
-//    }
-//
-//    return _accuracyMin;
-//    }
-//
-//    #pragma mark RSSI
-//    - (void)setRssi:(NSInteger)rssi
-//    {
-//    if (_rssi == rssi || rssi == 0) {
-//    return;
-//    }
-//
-//    _rssi = rssi;
-//    [self updateMaxRSSIWithRSSI:rssi];
-//    [self updateMinRSSIWithRSSI:rssi];
-//    }
-//
-//    - (void)updateMaxRSSIWithRSSI:(NSInteger)rssi
-//    {
-//    if (self.rssiMax < rssi) {
-//    self.rssiMax = rssi;
-//    }
-//    }
-//
-//    - (void)updateMinRSSIWithRSSI:(NSInteger)rssi
-//    {
-//    if (self.rssiMin > rssi) {
-//    self.rssiMin = rssi;
-//    }
-//    }
-//
-//    - (NSInteger)rssiMax
-//    {
-//    if (self.customRSSIMax) {
-//    return [self.customRSSIMax integerValue];
-//    }
-//    
-//    return _rssiMax;
-//    }
-//    
-//    - (NSInteger)rssiMin
-//    {
-//    if (self.customRSSIMin) {
-//    return [self.customRSSIMin integerValue];
-//    }
-//    
-//    return _rssiMin;
-//    }
 
 
 }
