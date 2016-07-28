d91605049f859d8f288fce1bb0fb2932d84f7f65
Lukasz Kuczborski <lukasz.kuczborski@allegrogroup.com>
2016-07-28 17:23:21 +0200

    Removes stub code.

--- Colors_Swift/Colors_Swift/ViewController.swift
+++ Colors_Swift/Colors_Swift/ViewController.swift
@@ -23,10 +23,6 @@ class ViewController: UIViewController {
         self.setupPorkHandler()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
     // MARK: PorkHandler
 
